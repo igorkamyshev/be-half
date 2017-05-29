47e5b7122a560e905f85d46ab3a2a708a4f3bfec
Igor Kamyshev <garik.novel@gmail.com>
2017-05-30 05:43:56 +0700

    fix fail with ;

--- src/AppBundle/Controller/TelegramController.php
+++ src/AppBundle/Controller/TelegramController.php
@@ -6,7 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class TelegramController extends Controller
 {
