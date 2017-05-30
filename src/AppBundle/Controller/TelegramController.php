2a69f5620c207ff28b5b163d29073f335f770c9c
Igor Kamyshev <garik.novel@gmail.com>
2017-05-30 23:27:14 +0700

    switch to other lin for telegram api

--- src/AppBundle/Controller/TelegramController.php
+++ src/AppBundle/Controller/TelegramController.php
@@ -9,8 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-use \TelegramBot\Api\Client as BotClient;
-use \TelegramBot\Api\Types\ReplyKeyboardMarkup as BotKeyboard;
+use Telegram\Bot\Api as TelegramApi;
 
 class TelegramController extends Controller
 {
@@ -25,38 +24,62 @@ class TelegramController extends Controller
             throw $this->createAccessDeniedException();
         }
 
-        $bot = new BotClient($telegramApiKey);
+        $telegram = new TelegramApi($telegramApiKey);
 
-        $controller = $this;
+        $request = $telegram->getWebhookUpdates();
 
-        $bot->command('start', function ($message) use ($bot, &$controller) {
-            $answer = 'Здравствуйте! Я – be-half, помогу вам следить за тратами "надвоих". Для начала создайте группу, или присоединитесь к существующей.';
+        $text = $request["message"]["text"];
+        $chatId = $request["message"]["chat"]["id"];
+        $name = $request["message"]["from"]["username"];
 
-            $keyboard = new BotKeyboard([['/create', '/join']], null, true);
+        $keyboard = [
+            ["Создать группу"],
+            ["Вступить в группу"],
+            ["Статистика"]
+        ];
 
-            $bot->sendMessage($message->getChat()->getId(), $answer, false, null, null, $keyboard);
-        });
+        if ($text) {
+            switch ($text) {
+                case '/start':
+                    $telegram->sendMessage([
+                        'chat_id' => $chatId,
+                        'text' => 'ok',
+                    ]);
+                    break;
+            }
+        } else {
 
-        $bot->command('create', function ($message) use ($bot, &$controller) {
-            /** @var User $user */
-            $user = $controller
-                ->getOrCreateUser($message->getChat()->getId());
+        }
 
-            $group = $controller
-                ->createGroup($user);
-
-            $answer = 'Группа создана (id ' . $group->getId() . '). Чтобы пригласить друга в группу отправьте ему id. Для создания транзакций отправляйте мне сообщения с суммой долга и комментарием (Напрмиер: 300 булочки на ужин).';
-
-            $bot->sendMessage($message->getChat()->getId(), $answer);
-        });
-
-        $bot->command('join', function ($message) use ($bot, &$controller) {
-            $answer = 'joined!';
-
-            $bot->sendMessage($message->getChat()->getId(), $answer);
-        });
-
-        $bot->run();
+//
+//        $bot->command('start', function ($message) use ($bot, &$controller) {
+//            $answer = 'Здравствуйте! Я – be-half, помогу вам следить за тратами "надвоих". Для начала создайте группу, или присоединитесь к существующей.';
+//
+//            $keyboard = new BotKeyboard([['/create', '/join']], null, true);
+//
+//            $bot->sendMessage($message->getChat()->getId(), $answer, false, null, null, $keyboard);
+//        });
+//
+//        $bot->command('create', function ($message) use ($bot, &$controller) {
+//            /** @var User $user */
+//            $user = $controller
+//                ->getOrCreateUser($message->getChat()->getId());
+//
+//            $group = $controller
+//                ->createGroup($user);
+//
+//            $answer = 'Группа создана (id ' . $group->getId() . '). Чтобы пригласить друга в группу отправьте ему id. Для создания транзакций отправляйте мне сообщения с суммой долга и комментарием (Напрмиер: 300 булочки на ужин).';
+//
+//            $bot->sendMessage($message->getChat()->getId(), $answer);
+//        });
+//
+//        $bot->command('join', function ($message) use ($bot, &$controller) {
+//            $answer = 'joined!';
+//
+//            $bot->sendMessage($message->getChat()->getId(), $answer);
+//        });
+//
+//        $bot->run();
 
         return new Response($apiKey);
     }
