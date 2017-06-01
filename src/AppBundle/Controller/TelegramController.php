61e49414afd8c670d665dcae390d7fe11ddb14fa
Igor Kamyshev <garik.novel@gmail.com>
2017-06-01 17:07:36 +0700

    switch to Utils for handle Telegram Api

--- src/AppBundle/Controller/TelegramController.php
+++ src/AppBundle/Controller/TelegramController.php
@@ -6,6 +6,7 @@ use AppBundle\Entity\Band;
 use AppBundle\Entity\Group;
 use AppBundle\Entity\User;
 use AppBundle\Utils\LoanManager;
+use AppBundle\Utils\TelegramBot;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,41 +16,29 @@ use Telegram\Bot\Api;
 
 class TelegramController extends Controller
 {
-    /* Command Constant */
-    const COMMAND_START = '/start';
-    const COMMAND_CREATE_BAND = 'создать группу';
-    const COMMAND_JOIN_BAND = 'вступить';
-    const COMMAND_STATUS = 'статус';
-
-    /** @var  Api */
-    private $telegram;
-
     /**
      * @Route("/telegram/{apiKey}", name="telegram")
      */
     public function telegramAction($apiKey)
     {
-        /** @var LoanManager $lm */
-        $lm = $this->get('loan_manager');
-
         $telegramApiKey = $this->getParameter('app.telegram_api_key');
 
         if ($apiKey != $telegramApiKey) {
             throw $this->createAccessDeniedException();
         }
 
-        $this->telegram = new Api($telegramApiKey);
+        /** @var Api $api */
+        $api = new Api($telegramApiKey);
 
-        $request = $this->telegram->getWebhookUpdates();
+        /** @var TelegramBot $bot */
+        $bot = $this->get('telegram_bot');
 
-        $text = mb_strtolower($request["message"]["text"]);
+        /** @var LoanManager $lm */
+        $lm = $this->get('loan_manager');
 
-        try {
-            $firstWord = substr($text, 0, strpos($text, " "));
-        } catch (\Exception $e) {
-            $firstWord = null;
-        }
+        $request = $api->getWebhookUpdates();
 
+        $text = $request["message"]["text"];
         $chatId = $request["message"]["chat"]["id"];
         $name = $request["message"]["from"]["username"];
 
@@ -58,148 +47,10 @@ class TelegramController extends Controller
             'name'   => $name,
         ]);
 
-        if ($text) {
-            switch ($text) {
-                case self::COMMAND_START:
-                    $this->handleStart($chatId);
-                    break;
-                case self::COMMAND_CREATE_BAND:
-                    $this->handleCreateBand($chatId, $user);
-                    break;
-                case self::COMMAND_STATUS:
-                    $this->handleStatus($chatId, $user);
-                    break;
-            }
-            switch ($firstWord) {
-                case self::COMMAND_JOIN_BAND:
-                    $this->handleJoinBand($chatId, $text, $user);
-                    break;
-            }
-        } else {
+        list($command, $params) = $bot->parseTextToCommand($text);
 
-        }
+        $response = $bot->handleCommand($command, $params, $user);
 
-        return new Response($apiKey);
-    }
-
-    private function handleStart($chatId)
-    {
-        $replyMarkup = $this->telegram->replyKeyboardMarkup([
-            'keyboard' => [
-                ["Создать группу"],
-            ],
-            'resize_keyboard' => true,
-            'one_time_keyboard' => true,
-        ]);
-        $this->telegram->sendMessage([
-            'chat_id'      => $chatId,
-            'text'         => 'Здравствуйте! Я – be-half, помогу следить за тратами "надвоих".',
-        ]);
-        $this->telegram->sendMessage([
-            'chat_id'      => $chatId,
-            'text'         => 'Для начала, создайте группу, или присоединитесь к существующей.',
-        ]);
-        $this->telegram->sendMessage([
-            'chat_id'      => $chatId,
-            'text'         => 'Чтобы присоедениться к группе отправьте мне: вступить #, где # – индивидуальный номер группы.',
-            'reply_markup' => $replyMarkup,
-        ]);
-    }
-
-    private function handleCreateBand($chatId, User $user)
-    {
-        $band = $user->getBand();
-
-        if ($band) {
-            $this->telegram->sendMessage([
-                'chat_id'      => $chatId,
-                'text'         => 'Вы уже состоите в группе! Индивидуальный номер – ' . $band->getId(),
-            ]);
-        } else {
-            /** @var LoanManager $lm */
-            $lm = $this->get('loan_manager');
-
-            $band = $lm->createBand($user);
-
-            $this->telegram->sendMessage([
-                'chat_id'      => $chatId,
-                'text'         => 'Группа создана! Индивидуальный номер – ' . $band->getId(),
-            ]);
-        }
-    }
-
-    private function handleJoinBand($chatId, $text, User $user) {
-        $band = $user->getBand();
-
-        if ($band) {
-            $this->telegram->sendMessage([
-                'chat_id'      => $chatId,
-                'text'         => 'Вы уже состоите в группе! Индивидуальный номер – ' . $band->getId(),
-            ]);
-        } else {
-            /** @var LoanManager $lm */
-            $lm = $this->get('loan_manager');
-
-            try {
-                $bandId = intval(substr($text, strpos($text, " ")));
-            } catch (\Exception $e) {
-                $bandId = 0;
-            }
-
-            $band = $this
-                ->getDoctrine()
-                ->getRepository(Band::class)
-                ->find($bandId);
-
-            if ($band) {
-                $band = $lm->joinBand($band, $user);
-
-                $this->telegram->sendMessage([
-                    'chat_id'      => $chatId,
-                    'text'         => 'Вы успешно присоеденились к группе!',
-                ]);
-            } else {
-                $this->telegram->sendMessage([
-                    'chat_id'      => $chatId,
-                    'text'         => 'Группы с таким индивидуальным номером не существует.',
-                ]);
-            }
-        }
-    }
-
-    private function handleStatus($chatId, User $user)
-    {
-        $band = $user->getBand();
-
-        if ($band) {
-            $this->telegram->sendMessage([
-                'chat_id'      => $chatId,
-                'text'         => 'Вы состоите в группе. Индивидуальный номер – ' . $band->getId(),
-            ]);
-        } else {
-            $this->telegram->sendMessage([
-                'chat_id'      => $chatId,
-                'text'         => 'Вы не состоите в группе.',
-            ]);
-        }
-
-        $balance = $user->getBalance();
-
-        if ($balance == 0) {
-            $this->telegram->sendMessage([
-                'chat_id'      => $chatId,
-                'text'         => 'Никто никому ничего не должен!',
-            ]);
-        } elseif ($balance > 0) {
-            $this->telegram->sendMessage([
-                'chat_id'      => $chatId,
-                'text'         => 'Вам должны ' . abs($balance) . ' руб.',
-            ]);
-        } elseif ($balance < 0) {
-            $this->telegram->sendMessage([
-                'chat_id'      => $chatId,
-                'text'         => 'Вы должны ' . abs($balance) . ' руб.',
-            ]);
-        }
+        return new Response(implode($params));
     }
 }
---
+++ src/AppBundle/Utils/TelegramBot.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace AppBundle\Utils;
+
+
+use AppBundle\Entity\User;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Telegram\Bot\Api;
+
+class TelegramBot
+{
+    /* Command Constant */
+    const COMMAND_START = '/start';
+    const COMMAND_CREATE_BAND = 'создать';
+    const COMMAND_LEAVE_BAND = 'покинуть';
+    const COMMAND_JOIN_BAND = 'вступить';
+    const COMMAND_STATUS = 'статус';
+    const COMMAND_HELP = 'помощь';
+    const COMMAND_NEW_TRANSACTION = 'transaction';
+
+    /** @var ContainerInterface */
+    private $container;
+
+    /** @var Api */
+    private $api;
+
+    /** @var LoanManager */
+    private $lm;
+
+    public function __construct(ContainerInterface $container, LoanManager $lm)
+    {
+        $this->container = $container;
+        $this->lm = $lm;
+
+        $this->api = new Api($this->container->getParameter('app.telegram_api_key'));
+    }
+
+    public function parseTextToCommand($text)
+    {
+        $text = mb_strtolower($text);
+
+        $separator = " ";
+
+        $words = array();
+        $tok = strtok($text, $separator);
+
+        while($tok) {
+            $words[] = $tok;
+            $tok = strtok(" \t\n");
+        }
+
+        if (is_numeric($words[0])) {
+            $command = self::COMMAND_NEW_TRANSACTION;
+            $params = [
+                'amount'  => $words[0],
+                'comment' => implode(' ', array_slice($words, 1)),
+            ];
+        } else {
+            $command = $words[0];
+            $params = array_slice($words, 1);
+        }
+
+        return [$command, $params];
+    }
+
+    public function handleCommand($command, array $params, User $user)
+    {
+        switch ($command) {
+            case self::COMMAND_START:
+                $this->handleHelpCommand($user);
+                break;
+            case self::COMMAND_CREATE_BAND:
+                break;
+            case self::COMMAND_JOIN_BAND:
+                break;
+            case self::COMMAND_LEAVE_BAND:
+                break;
+            case self::COMMAND_STATUS:
+                break;
+            case self::COMMAND_HELP:
+                $this->handleHelpCommand($user);
+                break;
+            case self::COMMAND_NEW_TRANSACTION:
+                break;
+        }
+
+        return true;
+    }
+
+    private function handleStartCommand(User $user)
+    {
+        return true;
+    }
+    private function handleHelpCommand(User $user)
+    {
+        $messages = [];
+
+        $messages[] = "Доступные команды:       
+        + помощь;
+        + создать группу;
+        + вступить #, где # – индивидуальный номер группы;
+        + покинуть группу;
+        + моя группа – выводит индивидуальный номер и членов группы;
+        + статус – выводит состояние счета.";
+
+        $messages[] = "Для создания транзакции отправьте мне сумму покупки и комментерий (необязательно).
+        Сумма будет разделена на два, результат будет записан на счет вашего друга, он получит уведомление.
+        
+        Пример:
+        
+        200 печенье
+        
+        На счет друга будет записано 100 рублей, он получит уведомление об этом с комментарием 'печенье'
+        ";
+
+        $this->sendMessagesToUser($user, $messages);
+
+        return true;
+    }
+
+    private function sendMessagesToUser(User $user, array $messages) {
+        foreach ($messages as $message) {
+            $this->api->sendMessage([
+                'chat_id'      => $user->getTelegramChatId(),
+                'text'         => $message,
+            ]);
+        }
+    }
+}
